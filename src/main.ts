eb7a0fbe73d38cce52449987db9e8c32d8d59ed8
Luiz Braz <brazluiz1000@gmail.com>
2026-05-27 01:19:24 -0300

    Adicionado estrutura HTML

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import "./style.css";
 import Slide from "./Slide";
+import imagem1 from "./assets/imagem_1.jpg"
+import imagem2 from "./assets/imagem_2.jpg"
+import imagem3 from "./assets/imagem_3.jpg"
+import video from "./assets/video.mp4"
+
 
 const app = document.querySelector<HTMLDivElement>("#app");
 
 if (app) {
   app.innerHTML = `
-
-
   <div class="carousel">
     <div class="slides">
-      <img src="./assets/imagem_1.jpg" >
-      <img src="./assets/imagem_2.jpg" >
-      <img src="./assets/imagem_3.jpg" >
+      <img src=${imagem1} >
+      <img src=${imagem2} >
+      <img src=${imagem3} >
+      <video src=${video} autoplay  controls></video>
     </div>
     <div class="controllers">
       <button class="prev">←</button>
